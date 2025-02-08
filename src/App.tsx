3055b2b260e9cbf6f090acb4d4cf924ce67723a0
Shouvik Ghosh <shouvikg.cal@gmail.com>
2025-02-08 23:16:19 +0530

    Add projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { Button } from "@mantine/core";
+import { Anchor, Box, List, Title } from "@mantine/core";
 
 function App() {
   return (
-    <Button>Hello</Button>
+    <Box p="sm">
+      <Box>
+        <Anchor href="/"
+          underline="never" c="black" size="1.5rem" fw="bold">Home</Anchor>
+      </Box>
+      <Box my="sm">
+        <Title size="h2">Projects</Title>
+        <List>
+          <List.Item><Anchor href="https://github.com/ShouvikGhosh2048/3d-cellular-automata">3D cellular automata</Anchor>: 3D cellular automata in Typescript and WebGL.</List.Item>
+          <List.Item><Anchor href="https://github.com/ShouvikGhosh2048/3d-geometry-topology-visualizer">3D geometry topology visualizer</Anchor>: A visualizer for 3d geometry and topology in Odin and Raylib.</List.Item>
+        </List>
+      </Box>
+    </Box>
   );
 }
 
